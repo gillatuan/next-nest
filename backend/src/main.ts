387407249aed0c9fee5376c725697d49a23b87ed
TuanBui <tuan.buidoanngoc@gmail.com>
2024-11-03 10:34:01 +0700

    update user dto add enable version set credential to cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,38 +1,45 @@
-import { NestFactory, Reflector } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { ValidationPipe } from '@nestjs/common';
-import { JwtAuthGuard } from './auth/passport/jwt-auth.guard';
+import { NestFactory, Reflector } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
-import { join } from 'path';
 import cookieParser from 'cookie-parser';
+import { join } from 'path';
+import { AppModule } from './app.module';
+import { JwtAuthGuard } from './auth/passport/jwt-auth.guard';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   const configService = app.get(ConfigService);
-  const reflector = app.get(Reflector)
+  const reflector = app.get(Reflector);
 
-  app.setGlobalPrefix('api/v1', { exclude: [''] });
-  app.setBaseViewsDir(join(__dirname, '..', 'views')) // view
-  
-  app.useStaticAssets(join(__dirname, '..', 'public')) // js, css, images
-  app.useGlobalGuards(new JwtAuthGuard(reflector))
-  app.useGlobalPipes(new ValidationPipe({
-    whitelist: true,
-    forbidNonWhitelisted: true
-  }));
+  app.setBaseViewsDir(join(__dirname, '..', 'views')); // view
+
+  app.useStaticAssets(join(__dirname, '..', 'public')); // js, css, images
+  app.useGlobalGuards(new JwtAuthGuard(reflector));
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
 
   //config cors
-  app.enableCors(
-    {
-      "origin": true,
-      "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
-      "preflightContinue": false,
-    }
-  );
+  app.enableCors({
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    credentials: true
+  });
 
   //config cookies
   app.use(cookieParser());
+
+  // version
+  app.enableVersioning({
+    type: VersioningType.URI,
+    prefix: 'api/v',
+    defaultVersion: ['1', '2'],
+  });
 
   await app.listen(configService.get<string>('PORT'));
 }
--- backend/src/modules/users/dto/create-user.dto.ts
+++ backend/src/modules/users/dto/create-user.dto.ts
@@ -1,47 +1,72 @@
-import { IsEmail, IsNotEmpty } from "class-validator";
+import { Type } from 'class-transformer';
+import {
+  IsEmail,
+  IsMongoId,
+  IsNotEmpty,
+  IsNotEmptyObject,
+  IsObject,
+  ValidateNested,
+} from 'class-validator';
+import mongoose from 'mongoose';
 
-export class CreateUserDto {
-    @IsNotEmpty({ message: "name không được để trống" })
-    name: string;
+class Company {
+  @IsNotEmpty()
+  _id: mongoose.Schema.Types.ObjectId;
 
-    @IsNotEmpty({ message: "email không được để trống" })
-    @IsEmail({}, { message: 'email không đúng định dạng' })
-    email: string;
-
-    @IsNotEmpty({ message: "password không được để trống" })
-    password: string;
-
-    @IsNotEmpty({ message: 'Phone ko dc trong' })
-    phone: string;
-  
-    @IsNotEmpty({ message: 'Address ko dc trong' })
-    address: string;
-    
-    image: string;
+  @IsNotEmpty()
+  name: string;
 }
 
+export class CreateUserDto {
+  @IsNotEmpty({ message: 'name không được để trống' })
+  name: string;
+
+  @IsNotEmpty({ message: 'email không được để trống' })
+  @IsEmail({}, { message: 'email không đúng định dạng' })
+  email: string;
+
+  @IsNotEmpty({ message: 'password không được để trống' })
+  password: string;
+
+  @IsNotEmpty({ message: 'Phone ko dc trong' })
+  phone: string;
+
+  @IsNotEmpty({ message: 'Address ko dc trong' })
+  address: string;
+
+  image: string;
+
+  @IsNotEmpty({ message: 'Role không được để trống' })
+  @IsMongoId({ message: 'Role có định dạng là mongo id' })
+  role: mongoose.Schema.Types.ObjectId;
+
+  @IsNotEmptyObject()
+  @IsObject()
+  @ValidateNested()
+  @Type(() => Company)
+  company: Company;
+}
 
 export class RegisterUserDto {
+  @IsNotEmpty({ message: 'Name không được để trống' })
+  name: string;
 
-    @IsNotEmpty({ message: 'Name không được để trống', })
-    name: string;
+  @IsEmail({}, { message: 'Email không đúng định dạng' })
+  @IsNotEmpty({ message: 'Email không được để trống' })
+  email: string;
 
-    @IsEmail({}, { message: 'Email không đúng định dạng', })
-    @IsNotEmpty({ message: 'Email không được để trống', })
-    email: string;
+  @IsNotEmpty({ message: 'Password không được để trống' })
+  password: string;
 
-    @IsNotEmpty({ message: 'Password không được để trống', })
-    password: string;
+  @IsNotEmpty({ message: 'Age không được để trống' })
+  age: number;
 
-    @IsNotEmpty({ message: 'Age không được để trống', })
-    age: number;
+  @IsNotEmpty({ message: 'Phone không được để trống' })
+  phone: number;
 
-    @IsNotEmpty({ message: 'Phone không được để trống', })
-    phone: number;
+  @IsNotEmpty({ message: 'Gender không được để trống' })
+  gender: string;
 
-    @IsNotEmpty({ message: 'Gender không được để trống', })
-    gender: string;
-
-    @IsNotEmpty({ message: 'Address không được để trống', })
-    address: string;
+  @IsNotEmpty({ message: 'Address không được để trống' })
+  address: string;
 }
